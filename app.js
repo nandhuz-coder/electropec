ebf4f1bbae600f86c2222b4b522a6f78d241c7e9
Nandhagopan <nandhagopan102@gamil.com>
2024-05-09 15:27:58 +0530

    hbs

--- app.js
+++ app.js
@@ -3,15 +3,25 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const hbs = require("express-handlebars");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 
-// *View engine setup
+//* view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutsDir: __dirname + "/views/layout/",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
 
 app.use(logger("dev"));
 app.use(express.json());
