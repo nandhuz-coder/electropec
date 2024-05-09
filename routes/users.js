75974551083695f67a73241332a8fc0d9961cac4
Nandhagopan <nandhagopan102@gamil.com>
2024-05-09 22:06:05 +0530

    added user page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+function ifuser(req, res, next) {
+  if (req.user) next();
+  else res.redirect("/");
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", ifuser, function (req, res, next) {
+  res.render("user/user");
 });
 
 module.exports = router;
